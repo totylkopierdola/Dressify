f69ffee38c544150b9f3f9ad9c2c7d4d8ffc9dd6
totylkopierdola <kudraj7@wp.pl>
2024-07-30 12:16:45 +0200

    navbar + font

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
